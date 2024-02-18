6e660492c64f46be4ea05689688d7ec2a2644c16
Kiran Indukuri <kiranpers@gmail.com>
2024-02-18 08:23:26 -0600

    Added Error Handling and Type Conversion program

--- console_input/main.go
+++ console_input/main.go
@@ -4,11 +4,25 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter Name: ")
+	// _ is added becuase we are adding a number
 	inputName, _ := reader.ReadString('\n')
 	fmt.Println("Hello ", inputName)
+
+	// to demo  error handling , and type conversion
+	fmt.Print("Enter Number: ")
+	numberInputString, _ := reader.ReadString('\n')
+	numberInput, err := strconv.ParseInt(strings.TrimSpace(numberInputString), 0, 64)
+	if err == nil {
+		fmt.Println("You Entered Number ", numberInput)
+	} else {
+		fmt.Println(err)
+	}
+
 }
